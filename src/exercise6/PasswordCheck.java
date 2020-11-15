eedc109124bba190994b6a714afad2b039bd0a9a
mladenivanov01 <mladenivanov01@gmail.com>
2020-11-15 09:02:46 +0000

    1

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
     }
 }
